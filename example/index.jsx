84463bd648db193563296a210f3a550aa3833faa
Boy with Silver Wings <agney@outlook.in>
2019-06-07 20:19:27 +0530

    setup form in example

--- example/index.jsx
+++ example/index.jsx
@@ -3,11 +3,29 @@ import ReactDOM from "react-dom";
 
 import useWebShare from "../src";
 
+function submitForm(target, actionFn) {
+  const title = target.title.value;
+  const text = target.text.value;
+  const url = target.url.value;
+  actionFn({
+    title, text, url
+  });
+}
+
 function App() {
-  const { isSupported, config, loading, share } = useWebShare();
+  const { isSupported, loading, share } = useWebShare();
   return (
     <main className="App">
-      { isSupported ? <p>Supported</p> : (
+      { !loading && isSupported ? (
+        <Fragment>
+          <form onSubmit={(event) => { event.preventDefault(); submitForm(event.target, share)}}>
+            <input type="text" placeholder="Title" name="title" />
+            <input type="text" placeholder="Text" name="text" />
+            <input type="text" placeholder="URL" name="url" />
+            <button type="submit">Share</button>
+          </form>
+        </Fragment>
+      ) : (
         <div style={{ textAlign: "center", paddingTop: "10%" }}>
           <p>
             <span>Your browser does not support </span>
