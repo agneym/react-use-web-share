69aae2ffdc61cbd1f1d181951cf003e13b8c4961
Boy with Silver Wings <agney@outlook.in>
2019-06-07 19:32:10 +0530

    add doc for get url

--- src/get-url.ts
+++ src/get-url.ts
@@ -1,3 +1,8 @@
+/**
+ * Get the URL to be shared.
+ * If the site uses canonical URL, then use that URL otherwise the current URL
+ * @param url URL that might be passed on by the user
+ */
 function getUrl(url?: string) {
   if(!!url) {
     return url;
